e5125184f0ce69f44874fe72d60f09c82190d169
ksato <ksapr02@gmail.com>
2021-06-08 00:58:54 +0900

    seleniumにて必要なデータを取得

--- rcard-to-slack/app.py
+++ rcard-to-slack/app.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 import os
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
 def lambda_handler(event, context):
@@ -9,8 +10,13 @@ def lambda_handler(event, context):
     ID = os.environ['RCARD_ID']
     PASS = os.environ['RCARD_PASS']
 
+    options = Options()
+    options.add_argument('--headless')
     driver = webdriver.Chrome(
-        executable_path="/Users/kentasato/Documents/chromedriver")
+        executable_path="/Users/kentasato/Documents/chromedriver",  # TODO: デプロイ後のパスに修正
+        options=options)
+
+    # ログイン
     driver.get("https://www.rakuten-card.co.jp/e-navi/")
 
     id_form = driver.find_element_by_id("u")
@@ -22,6 +28,12 @@ def lambda_handler(event, context):
     login_button = driver.find_element_by_id("loginButton")
     login_button.click()
 
-    amount = driver.find_element_by_id("rf-tera")
-    print(1111111111111111)
-    print(amount)
+    # データ取得
+    amount = driver.find_elements_by_class_name("rf-tera")[0].text
+    left_point = driver.find_elements_by_class_name("rf-red")[7].text
+    expected_point = driver.find_elements_by_class_name("rf-red")[9].text
+
+    # TODO: Slackへ連携
+    text = '支払い予定金額：' + amount + '¥n'
+    + 'ポイント残高：' + left_point + '¥n'
+    + '獲得予定ポイント：' + expected_point
